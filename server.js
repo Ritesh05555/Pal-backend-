4abd5867aa565bc7beece04a1978baa67837a6f3
RITESH SAHANI <shubamsahani03@gmail.com>
2025-04-14 21:20:50 +0530

    test-1

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const app = express();
 // Connect to MongoDB first
 connectDB().then(() => {
   // Middleware
-  app.use(cors({ origin: 'https://pal-frontend.onrender.com', credentials: true }));
+  app.use(cors({ origin: 'https://aapka-pal.onrender.com' , credentials: true }));
   app.use(express.json());
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
